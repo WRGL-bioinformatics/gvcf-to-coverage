57be26cab0fb341aa7dfb9292297ee8ccd5d773c
B.Sanders <ben@bensanders.co.uk>
2020-05-11 12:05:10 +0100

    added support for gzipped vcfs

--- gvcf_to_summary.sh
+++ gvcf_to_summary.sh
@@ -14,7 +14,6 @@ if [ "$1" = "-h" -o "$1" = "--help" ]; then
 	exit 1
 fi
 
-
 # 20x minimum depth is required
 # DEV: maybe spin out into a config file for clarity??
 mindp=20
@@ -39,8 +38,10 @@ if [ ! -f "$vcf" ]; then
 fi
 
 # output filenames are derived from VCF file name
-out="$( basename $vcf .vcf )".summary.txt
-depthout="$( basename $vcf .vcf )".coverage.txt
+out="$( basename $vcf .gvcf.gz )"
+out="$( basename $out .vcf )".summary.txt
+depthout="$( basename $vcf .gvcf.gz )"
+depthout="$( basename $depthout .vcf )".coverage.txt
 # quickly open the VCF file to get the sample ID
 sampleid=$( grep -v "^##" "$vcf" | grep "^#" | cut -f 10 )
 
@@ -49,14 +50,28 @@ echo "## Minimum depth of coverage: $mindp" >> "$out"
 echo -e "#GENE\tLENGTH\tCOVERED\tCOVERAGE" >> "$out"
 
 # Convert the BP resolution gVCF to a whole-gene summary
-cat "$vcf" | \
-python2 "$script_path"/gvcf_to_bed.py "$mindp" | \
-bedtools map -g /scratch/WRGL/REFERENCE_FILES/REFERENCE_GENOME/GRCh37_no_gl000201.genome -a "$bed" -b stdin -c 5 -o count -null 0 | \
-python2 "$script_path"/gene_summariser.py >> "$out"
+if (file "$vcf" | grep -q compressed ) ; then
+    zcat "$vcf" | \
+    python2 "$script_path"/gvcf_to_bed.py "$mindp" | \
+    bedtools map -g /scratch/WRGL/REFERENCE_FILES/REFERENCE_GENOME/GRCh37_no_gl000201.genome -a "$bed" -b stdin -c 5 -o count -null 0 | \
+    python2 "$script_path"/gene_summariser.py >> "$out"
+else
+    cat "$vcf" | \
+    python2 "$script_path"/gvcf_to_bed.py "$mindp" | \
+    bedtools map -g /scratch/WRGL/REFERENCE_FILES/REFERENCE_GENOME/GRCh37_no_gl000201.genome -a "$bed" -b stdin -c 5 -o count -null 0 | \
+    python2 "$script_path"/gene_summariser.py >> "$out"
+fi
 
 # Also create a samtools depth style report for pipeline dowload
 # Set minimum depth to 0 to ensure depth is output for all target positions
-cat "$vcf" | \
-python2 "$script_path"/gvcf_to_bed.py 0 | \
-bedtools intersect -u -sorted -g /scratch/WRGL/REFERENCE_FILES/REFERENCE_GENOME/GRCh37_no_gl000201.genome -a stdin -b "$bed" | \
-cut -f 1,3,5 > "$depthout"
+if (file "$vcf" | grep -q compressed ) ; then
+    zcat "$vcf" | \
+    python2 "$script_path"/gvcf_to_bed.py 0 | \
+    bedtools intersect -u -sorted -g /scratch/WRGL/REFERENCE_FILES/REFERENCE_GENOME/GRCh37_no_gl000201.genome -a stdin -b "$bed" | \
+    cut -f 1,3,5 > "$depthout"
+else
+    cat "$vcf" | \
+    python2 "$script_path"/gvcf_to_bed.py 0 | \
+    bedtools intersect -u -sorted -g /scratch/WRGL/REFERENCE_FILES/REFERENCE_GENOME/GRCh37_no_gl000201.genome -a stdin -b "$bed" | \
+    cut -f 1,3,5 > "$depthout"
+fi
